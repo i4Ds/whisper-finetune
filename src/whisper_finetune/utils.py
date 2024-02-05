72da43944164c8e2f5418735124e26a54d2943ed
Vincenzo <vincenzo.timmel@fhnw.ch>
2024-02-05 19:44:03 +0100

    Added distributed training, if applicable.

--- src/whisper_finetune/utils.py
+++ src/whisper_finetune/utils.py
@@ -2,6 +2,7 @@ import random
 
 import numpy as np
 import torch
+import torch.distributed as dist
 import yaml
 
 
@@ -15,3 +16,17 @@ def set_seed(seed: int):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
+
+
+def distributed_setup(rank, world_size):
+    if not dist.is_available() or world_size < 2:
+        print("Distributed training is not available or world size is less than 2. World Size: ", world_size)
+        return False
+    # initialize the process group
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    if dist.is_initialized():
+        print(f"Rank {rank} initialized.")
+        return True
+    else:
+        print(f"Rank {rank} failed to initialize.")
+        return False
