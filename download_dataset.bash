8e21d9f54971297460327bd32d1a5313ae8b4019
Vincenzo Timmel <vincenzo.timmel@fhnw.ch>
2024-02-07 12:11:45 +0100

    ADded sbatch to download datasets on scicore login node.

---
+++ download_dataset.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define the dataset to download and your Hugging Face token
+DATASET_NAME="i4ds/SDS_STT_SPC_mixed"
+
+# Create a virtual environment in the current directory
+python3 -m venv .hu_ds_download
+
+# Get env variables
+export $(cat .env | xargs)
+
+# Activate the virtual environment
+source .hu_ds_download/bin/activate
+
+# Update pip to its latest version
+pip install --upgrade pip
+
+# Install the datasets library by Hugging Face
+pip install datasets
+
+# Use the load_dataset function from the datasets library
+python3 -c "from datasets import load_dataset; dataset = load_dataset('$DATASET_NAME')"
+
+# Deactivate the virtual environment when done
+deactivate
