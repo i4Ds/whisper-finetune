3bea46ef9287849b6bc45c480d4ab1eff072115c
Vincenzo Timmel <timmel0000@login01.cluster.bc2.ch>
2024-04-10 15:30:11 +0200

    Archived old yamls.

--- sc_sbatch.sh
+++ sc_sbatch.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=whisper_finetune     # create a short name for your job
 #SBATCH --cpus-per-task=8                  #This is the number of cores reserved
 #SBATCH --mem-per-cpu=8G              #This is the memory reserved per core.
-#SBATCH --time=24:00:00         # total run time limit (HH:MM:SS)
+#SBATCH --time=72:00:00         # total run time limit (HH:MM:SS)
 #SBATCH --partition=a100       # or titanx
 #SBATCH --gres=gpu:1            # number of gpus per node
-#SBATCH --qos=1day	           # qos level
+#SBATCH --qos=1week	           # qos level
 
 # ACTIVATE ANACONDA
 eval "$(conda shell.bash hook)"
@@ -14,4 +14,4 @@ conda activate whisper_finetune
 # Get env variables
 export $(cat .env | xargs)
 
-python src/whisper_finetune/scripts/finetune.py --config configs/tiny-lora-sg-corpus.yaml
+python src/whisper_finetune/scripts/finetune.py --config configs/large-lora-decoder-mp-sg-corpus.yaml
