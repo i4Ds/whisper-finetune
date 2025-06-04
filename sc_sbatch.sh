a7971bbbb453b9a2e6c14172a626ba0340077735
kenfus <vincenzo.timmel@gmail.com>
2025-06-05 00:09:48 +0200

    Updated back to smaller GPU and thus smaller batch size.

--- sc_sbatch.sh
+++ sc_sbatch.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=whisper_finetune_${CONFIG_NAME:-job}   # job name (we'll override CONFIG_NAME below)
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=16G
-#SBATCH --time=48:00:00
-#SBATCH --partition=a100
+#SBATCH --time=47:00:00
+#SBATCH --partition=a100      # or titanx
 #SBATCH --gres=gpu:1
 #SBATCH --qos=gpu1week
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
