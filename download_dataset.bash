c614d8c6dab0acbd71e4f38c613765561855a520
kenfus <vincenzo.timmel@gmail.com>
2025-08-29 15:11:51 +0200

    Bumb config.

--- download_dataset.bash
+++ download_dataset.bash
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Define the datasets to download
-DATASET_NAMES=("i4ds/spc_r_whisper")
-
-# Create a virtual environment in the current directory
-python3 -m venv .hu_ds_download
+DATASET_NAMES=("i4ds/srg_v3_real_pl_train" "i4ds/srg_v3_real_pl_valid")
 
 # Get env variables
 export $(cat .env | xargs)
@@ -13,19 +10,7 @@ export $(cat .env | xargs)
 export HF_DATASETS_OFFLINE=0
 export TRANSFORMERS_OFFLINE=0
 
-# Activate the virtual environment
-source .hu_ds_download/bin/activate
-
-# Update pip to its latest version
-pip install --upgrade pip
-
-# Install the datasets library by Hugging Face
-pip install datasets
-
 # Loop through the datasets and download each one
 for DATASET_NAME in "${DATASET_NAMES[@]}"; do
-    python3 -c "from datasets import load_dataset; dataset = load_dataset('$DATASET_NAME')"
+    python3 -c "from datasets import load_dataset; dataset = load_dataset('$DATASET_NAME', download_mode='force_redownload')"
 done
-
-# Deactivate the virtual environment when done
-deactivate
